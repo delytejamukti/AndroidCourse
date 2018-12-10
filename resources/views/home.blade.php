a026476d862edf110c96642f9bd52f866f15a7f8
kyou <kyoudaneko@gmail.com>
2018-12-10 21:32:11 +0700

    update: home + advance
    update: home + advance

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,4 +20,50 @@
         </div>
     </div>
 </div>
+
+<!-- start content-->
+<ul class="nav nav-tabs mb-3">
+    <li class="nav-item">
+        <a class="nav-link active" data-toggle="tab" href="#beg">Beginner</a>
+    </li>
+</ul>
+
+<div class="tab-content">
+    <!-- Beg -->
+    <div class="tab-pane fade show active" id="beg">
+        <p>Beginner</p>
+        <div class="container">
+            <div class="row p-3">
+                <div class="" style="width: 100%;">
+                    <h1>TITLE</h1>
+                </div>
+                <div class="" style="width: 100%;height: 300px;">
+                    <video style="width: 400px;height: 300px" controls>
+                        <source src="mov_bbb.mp4" type="video/mp4">
+                        Your browser does not support HTML5 video.
+                    </video>
+                </div>
+                <div class="" style="width: 100%;max-height: 300px; overflow: hidden;">
+                    TEXTasd asdas das dasdasa asddd ddd ddd dddddas  ddd  dd ddd  dd ddddd dddd ddddddd dd  ddddsdasdasd asd asd asd as dasd asd asda sdasdasdas das dasd asdasdasd asd asd asdasdas asddd ddd ddd dddddas  ddd  dd ddd  dd ddddd dddd ddddddd dd  ddddsdasdasd asd asd asd as dasd asd asda sdasdasdas das dasd asdasdasd asd asd asdasdas asddd ddd ddd dddddas  ddd  dd ddd  dd ddddd dddd ddddddd dd  ddddsdasdasd asd asd asd as dasd asd asda sdasdasdas das dasd asdasdasd asd asd asdasdas
+                </div>
+            </div>
+            <div class="row p-3">
+                <div class="" style="width: 100%;">
+                    <h1>TITLE</h1>
+                </div>
+                <div class="" style="width: 100%;height: 300px;">
+                    <video style="width: 400px;height: 300px" controls>
+                        <source src="mov_bbb.mp4" type="video/mp4">
+                        Your browser does not support HTML5 video.
+                    </video>
+                </div>
+                <div class="" style="width: 100%;max-height: 300px; overflow: hidden;">
+                    TEXTasd asdas das dasdasa asddd ddd ddd dddddas  ddd  dd ddd  dd ddddd dddd ddddddd dd  ddddsdasdasd asd asd asd as dasd asd asda sdasdasdas das dasd asdasdasd asd asd asdasdas asddd ddd ddd dddddas  ddd  dd ddd  dd ddddd dddd ddddddd dd  ddddsdasdasd asd asd asd as dasd asd asda sdasdasdas das dasd asdasdasd asd asd asdasdas asddd ddd ddd dddddas  ddd  dd ddd  dd ddddd dddd ddddddd dd  ddddsdasdasd asd asd asd as dasd asd asda sdasdasdas das dasd asdasdasd asd asd asdasdas
+                </div>
+            </div>
+        </div>
+    </div>
+
+  </div>
+<!-- end content -->
 @endsection
