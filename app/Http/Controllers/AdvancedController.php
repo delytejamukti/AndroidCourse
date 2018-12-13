03b83787c1c2ec1f17215482474bd2ebd18cbde6
Ahmad Ibnu Fajar <ahmadibnu-fajar@hotmail.com>
2018-12-13 20:29:44 +0700

    Update AdvancedController.php

--- app/Http/Controllers/AdvancedController.php
+++ app/Http/Controllers/AdvancedController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Courses;
+use Auth;
 
 class AdvancedController extends Controller
 {
@@ -13,6 +15,7 @@ class AdvancedController extends Controller
 
     public function index()
     {
-        return view('advance');
+        $data['courses'] = Courses::where('is_advanced',Auth::user()->advanced)->get();
+        return view('advance', $data);
     }
 }
