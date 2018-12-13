653eac43c94a506109cf477166980b1c04fdec5f
delyteja <tejamukti03@gmail.com>
2018-12-13 15:57:28 +0700

    nambahi seeder

--- database/seeds/CoursesTableSeeder.php
+++ database/seeds/CoursesTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Factory;
 
 class CoursesTableSeeder extends Seeder
 {
@@ -11,6 +12,24 @@ class CoursesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
+        DB::table('courses')->truncate();
+
+        $courses = [];
+        $faker = Factory::create();
+
+        for($i = 1; $i <=10; $i++)
+        {
+            $courses[] = [
+                'is_advanced' => rand(0,1),
+                'judul' => $faker->sentence(rand(7,10)),
+                'diskripsi' => $faker->text(rand(100,150)),
+                'nama_vidio' => $faker->sentence(rand(7,10)),
+            ];
+        }
+
+        DB::table('courses')->insert($courses);
+ 
     }
 }
--- database/seeds/HistoryViewTableSeeder.php
+++ database/seeds/HistoryViewTableSeeder.php
@@ -11,6 +11,28 @@ class HistoryViewTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        \DB::table('history_view')->delete();
+        \DB::table('history_view')->insert(array (
+            array (
+                'id_user' => '1',
+                'id_vidio' => '1',
+            ),
+            array (
+                'id_user' => '1',
+                'id_vidio' => '2',
+            ),
+            array (
+                'id_user' => '1',
+                'id_vidio' => '3',
+            ),
+            array (
+                'id_user' => '1',
+                'id_vidio' => '4',
+            ),
+            array (
+                'id_user' => '1',
+                'id_vidio' => '5',
+            ),
+        ));    
     }
 }
